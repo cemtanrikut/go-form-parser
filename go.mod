705ee352a9b0f4bc1edb41defcdca93746a55205
Cem Tanrikut <cemtanrikut@gmail.com>
2025-02-11 21:03:57 +0100

    added pdf generator

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main.go
+
+go 1.22.1
+
+require github.com/jung-kurt/gofpdf v1.16.2
---
+++ internal/pdf/pdf_generator.go
@@ -0,0 +1,38 @@
+package pdf
+
+import (
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+	"main.go/internal/parser"
+)
+
+// GeneratePDF takes a parsed Form and creates a PDF representation
+func GeneratePDF(form *parser.Form, outputFilename string) error {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, "Form Submission")
+	pdf.Ln(10)
+
+	pdf.SetFont("Arial", "", 12)
+
+	// Process fields
+	for _, field := range form.Fields {
+		pdf.Cell(40, 10, fmt.Sprintf("%s: %s", field.Caption, field.Type))
+		pdf.Ln(8)
+	}
+
+	// Process sections
+	for _, section := range form.Sections {
+		pdf.SetFont("Arial", "B", 14)
+		pdf.Cell(40, 10, section.Title)
+		pdf.Ln(8)
+		pdf.SetFont("Arial", "", 12)
+		for _, field := range section.Contents {
+			pdf.Cell(40, 10, fmt.Sprintf("%s: %s", field.Caption, field.Type))
+			pdf.Ln(8)
+		}
+	}
+	return pdf.OutputFileAndClose(outputFilename)
+}

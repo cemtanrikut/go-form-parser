00922088e2e964b51c843042008445374aadb3fa
Cem Tanrikut <cemtanrikut@gmail.com>
2025-02-11 21:06:19 +0100

    added parser and generator in main file

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"main.go/internal/parser"
+	"main.go/internal/pdf"
+)
+
+func main() {
+	form, err := parser.ParseXML("form.xml")
+	if err != nil {
+		log.Fatalf("Error parsing XML: %s", err)
+	}
+
+	err = pdf.GeneratePDF(form, "form_output.pdf")
+	if err != nil {
+		log.Fatalf("Error generating PDF: %s", err)
+	}
+
+	fmt.Println("PDF generated successfully. Check output/form_output.pdf")
+}
